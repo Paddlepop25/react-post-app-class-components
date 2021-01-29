a13876068318f9295b3c6dd0faac3356ffcc2cbd
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-01-29 14:38:43 +0800

    added fake data in app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,39 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  state = {
+    posts: [
+      {
+        userId: 1,
+        id: 1,
+        title: 'rice',
+        body: 'fried rice, claypot chicken rice, brown porridge, briyani',
+      },
+      {
+        userId: 1,
+        id: 2,
+        title: 'noodles',
+        body: 'hokkien mee, mee goreng, prawn noodles, bak chor mee, pho',
+      },
+      {
+        userId: 1,
+        id: 3,
+        title: 'bread',
+        body: 'french loaf, sandwich, yakun kaya toast, butter bun',
+      },
+    ],
+  }
   render() {
     return (
       <>
-        <h3>Hello world!</h3>
+        {this.state.posts.map((post) => {
+          return (
+            <div>
+              <h3>{post.title}</h3>
+              <code>{post.body}</code>
+            </div>
+          )
+        })}
       </>
     )
   }
