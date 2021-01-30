f15098cd4565b54f66d1de3c5c8cee00706e93e6
Paddlepop25 <shecodeshsucodes18@gmail.com>
2021-01-30 12:29:48 +0800

    moved fake data posts data from app.js to posts.json

--- src/App.js
+++ src/App.js
@@ -2,33 +2,19 @@ import React, { Component } from 'react'
 import Posts from './components/Posts'
 import AddPost from './components/AddPost'
 import Header from './components/Header'
-import Photos from './components/Photos'
-import { Link } from 'react-router-dom'
 
+import posts from './posts.json'
 import './App.css'
 
 class App extends Component {
   state = {
-    posts: [
-      {
-        userId: 1,
-        id: 1,
-        title: 'rice',
-        body: 'fried rice, claypot chicken rice, brown porridge, briyani',
-      },
-      {
-        userId: 1,
-        id: 2,
-        title: 'noodles',
-        body: 'hokkien mee, prawn noodles, bak chor mee, pho',
-      },
-      {
-        userId: 1,
-        id: 3,
-        title: 'bread',
-        body: 'french loaf, sandwich, yakun kaya toast, butter bun',
-      },
-    ],
+    posts: [],
+  }
+
+  componentDidMount() {
+    this.setState({
+      posts,
+    })
   }
 
   addPost = (post) => {
--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import Header from './Header'
 
 class EditPost extends Component {
   render() {
     return (
       <>
+        <Header />
         <h3>Edit post</h3>
       </>
     )
